ff0ddc015c677c70a4acd2480d9005a6d1ebdd2c
iloveflo <binha10k56@gmail.com>
2025-04-19 19:11:04 +0700

    đã thêm hướng dẫn đặt hàng và chọn size

--- javascript/dropdown.js
+++ javascript/dropdown.js
@@ -22,3 +22,14 @@ window.onclick = function(event) {
         }
     }
 }
+function openModal(imageSrc) {
+    const modal = document.getElementById("imageModal");
+    const modalImg = document.getElementById("modalImage");
+
+    modalImg.src = imageSrc;
+    modal.style.display = "flex";
+}
+
+function closeModal() {
+    document.getElementById("imageModal").style.display = "none";
+}
